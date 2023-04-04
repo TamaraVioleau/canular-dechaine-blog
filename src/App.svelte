b8b60b865c05be927458b899aa285e59e3a3bacb
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-04 11:27:22 +0200

    creation branch feature/footer, construction html.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   import PageProfilAuthors from "./lib/PageProfilAuthors.svelte";
+  import PageProfilMembers from "./lib/PageProfilMembers.svelte";
 import Register from "./lib/Register.svelte";
+import Footer from "./lib/Footer.svelte";
+
 </script>
 
-<PageProfilAuthors/>
-
+<Footer/>
 
 <style lang="scss">
     </style>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,32 @@
+<footer>
+  <button id="button__contact"><a href="/">Contact</a></button>
+  <div class="social">
+    <button><a href=""><i class="fa-brands fa-facebook" /></a></button>
+    <button><a href=""><i class="fa-brands fa-twitter" /></a></button>
+    <button><a href=""><i class="fa-brands fa-instagram" /></a></button>
+  </div>
+  <ul>
+    <li>Mentions légales</li>
+    <li>Politique de confidentialité</li>
+    <li>A propos</li>
+  </ul>
+</footer>
+
+<style lang="scss">
+  @import "../utils/extends";
+  @import "../utils/mixins";
+  @import "../utils/variables";
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    #button__contact {
+    }
+
+    .social {
+      display: flex;
+      gap: 1rem;
+    }
+  }
+</style>
