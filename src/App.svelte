74b47c20ce3564bc8255b92a3456ee0638a85b1c
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-04 09:56:09 +0200

    Just a modification of the app page to see the component pages.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
   import PageProfilAuthors from "./lib/PageProfilAuthors.svelte";
-  import PageProfilMembers from "./lib/PageProfilMembers.svelte";
 import Register from "./lib/Register.svelte";
 </script>
 
-<Register/>
+<PageProfilAuthors/>
 
 
 <style lang="scss">
