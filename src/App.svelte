1299531ed002054533a4c783e51ad67671422fbc
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-04 22:13:06 +0200

    construction PageArticles.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,20 @@
 <script>
   import Footer from "./lib/Footer.svelte";
 import Homepage from "./lib/HomePage.svelte"; 
+  import PageArticles from "./lib/PageArticles.svelte";
   import PageProfilAuthors from "./lib/PageProfilAuthors.svelte";
   import PageProfilMembers from "./lib/PageProfilMembers.svelte";
   import Register from "./lib/Register.svelte";
   import SearchPage from "./lib/SearchPage.svelte";
 </script>
 
-<SearchPage/>
-
-<!-- <Homepage/>
+<PageArticles/>
+<Homepage/>
+  <SearchPage/>
 <PageProfilAuthors/>
 <PageProfilMembers/>
 <Register/>
+<!-- 
 
  -->
 <Footer/>
