1e7a97ffe705ca4a99c568f19ac16723a6447f96
FredMorino <fred.morino73@gmail.com>
2023-04-05 09:14:21 +0200

    update page dev

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script>
   import Footer from "./lib/Footer.svelte";
-import Homepage from "./lib/HomePage.svelte"; 
+  import Homepage from "./lib/HomePage.svelte";
   import PageProfilAuthors from "./lib/PageProfilAuthors.svelte";
   import PageProfilMembers from "./lib/PageProfilMembers.svelte";
   import Register from "./lib/Register.svelte";
   import SearchPage from "./lib/SearchPage.svelte";
 </script>
 
-<SearchPage/>
+<SearchPage />
 
 <!-- <Homepage/>
 <PageProfilAuthors/>
@@ -15,5 +15,4 @@ import Homepage from "./lib/HomePage.svelte";
 <Register/>
 
  -->
-<Footer/>
-
+<Footer />
