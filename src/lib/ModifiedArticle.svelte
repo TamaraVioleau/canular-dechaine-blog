603c7e0475c1a16858de400caf35dbbcfe38c42b
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-05 10:37:16 +0200

    correction ModifiedArticle.svelte

--- src/lib/ModifiedArticle.svelte
+++ src/lib/ModifiedArticle.svelte
@@ -1,4 +1,5 @@
 <script>
+  //SCRIPT COMPTEUR DE MOTS
   let myMaxLength = 500;
 
   // Définir le seuil d'alerte pour informer l'utilisateur du nombre de caractères restants
@@ -18,131 +19,119 @@
     // Calculer les caractères restants en soustrayant le nombre de caractères actuels de la longueur maximale autorisée
     charactersRemaining = myMaxLength - characterCount;
   };
+
+  //SCRIPT PREVIEW TEXTAREA
+  let text = "";
+  function updatePreview() {
+    const preview = document.getElementById("preview");
+    preview.innerHTML = text;
+  }
 </script>
 
 <main>
-  <div class="wrap">
-    <section class="picture">
-      <img src="https://picsum.photos/200" alt="foto" />
-    </section>
-    <div class="article">
-      <h2>TITRE ARTICLE</h2>
-      <p aria-label="Texte de l'article">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi
-        aliquid magnam aspernatur, porro iure in incidunt magni nulla? Tempora
-        doloremque molestiae magnam nisi ex dolores delectus atque enim et.
-        Neque ad impedit porro beatae blanditiis ab saepe reiciendis alias
-        dignissimos esse deserunt quasi iure repellendus molestias animi
-        distinctio fuga asperiores iusto, magnam, id, dicta voluptas! Delectus
-        voluptates tempora quibusdam. Non quaerat omnis magnam obcaecati
-        voluptatum ut recusandae consequuntur nemo ad. Pariatur quas nisi quod
-        optio est cupiditate praesentium distinctio similique nemo impedit!
-        Atque ea consequuntur eos id ipsa fugit. Nostrum perspiciatis sit
-        similique cum placeat doloremque quod dolores voluptas harum tenetur, id
-        nihil adipisci suscipit temporibus, quidem aliquam voluptates.
-        dignissimos esse deserunt quasi iure repellendus molestias animi
-        distinctio fuga asperiores iusto, magnam, id, dicta voluptas! Delectus
-        voluptates tempora quibusdam. Non quaerat omnis magnam obcaecati
-        voluptatum ut recusandae consequuntur nemo ad. Pariatur quas nisi quod
-        optio est cupiditate praesentium distinctio similique nemo impedit!
-        Atque ea consequuntur eos id ipsa fugit. Nostrum perspiciatis sit
-        similique cum placeat doloremque quod dolores voluptas harum tenetur, id
-        nihil adipisci suscipit temporibus, quidem aliquam voluptates.
-        optio est cupiditate praesentium distinctio similique nemo impedit!
-        Atque ea consequuntur eos id ipsa fugit. Nostrum perspiciatis sit
-        similique cum placeat doloremque quod dolores voluptas harum tenetur, id
-        nihil adipisci suscipit temporibus, quidem aliquam voluptates.
-        dignissimos esse deserunt quasi iure repellendus molestias animi
-        distinctio fuga asperiores iusto, magnam, id, dicta voluptas! Delectus
-        voluptates tempora quibusdam. Non quaerat omnis magnam obcaecati
-        voluptatum ut recusandae consequuntur nemo ad. Pariatur quas nisi quod
-        optio est cupiditate praesentium distinctio similique nemo impedit!
-        Atque ea consequuntur eos id ipsa fugit. Nostrum perspiciatis sit
-        similique cum placeat doloremque quod dolores voluptas harum tenetur, id
-        nihil adipisci suscipit temporibus, quidem aliquam voluptates.
-      </p>
-    </div>
+  <article class="article">
+    <img src="https://picsum.photos/200" alt="foto" />
+    <h2>TITRE ARTICLE</h2>
+    <p id="preview" aria-label="Texte de l'article">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi
+      aliquid magnam aspernatur, porro iure in incidunt magni nulla? Tempora
+      doloremque molestiae magnam nisi ex dolores delectus atque enim et. Neque
+      ad impedit porro beatae blanditiis ab saepe reiciendis alias dignissimos
+      esse deserunt quasi iure repellendus molestias animi distinctio fuga
+      asperiores iusto, magnam, id, dicta voluptas! Delectus voluptates tempora
+      quibusdam. Non quaerat omnis magnam obcaecati voluptatum ut recusandae
+      consequuntur nemo ad. Pariatur quas nisi quod optio est cupiditate
+      praesentium distinctio similique nemo impedit! Atque ea consequuntur eos
+      id ipsa fugit. Nostrum perspiciatis sit similique cum placeat doloremque
+      quod dolores voluptas harum tenetur, id nihil adipisci suscipit
+      temporibus, quidem aliquam voluptates. dignissimos esse deserunt quasi
+      iure repellendus molestias animi distinctio fuga asperiores iusto, magnam,
+      id, dicta voluptas! Delectus voluptates tempora quibusdam. Non quaerat
+      omnis magnam obcaecati voluptatum ut recusandae consequuntur nemo ad.
+      Pariatur quas nisi quod optio est cupiditate praesentium distinctio
+      similique nemo impedit! Atque ea consequuntur eos id ipsa fugit. Nostrum
+      perspiciatis sit similique cum placeat doloremque quod dolores voluptas
+      harum tenetur, id nihil adipisci suscipit temporibus, quidem aliquam
+      voluptates. optio est cupiditate praesentium distinctio similique nemo
+      impedit! Atque ea consequuntur eos id ipsa fugit. Nostrum perspiciatis sit
+      similique cum placeat doloremque quod dolores voluptas harum tenetur, id
+      nihil adipisci suscipit temporibus, quidem aliquam voluptates. dignissimos
+      esse deserunt quasi iure repellendus molestias animi distinctio fuga
+      asperiores iusto, magnam, id, dicta voluptas! Delectus voluptates tempora
+      quibusdam. Non quaerat omnis magnam obcaecati voluptatum ut recusandae
+      consequuntur nemo ad. Pariatur quas nisi quod optio est cupiditate
+      praesentium distinctio similique nemo impedit! Atque ea consequuntur eos
+      id ipsa fugit. Nostrum perspiciatis sit similique cum placeat doloremque
+      quod dolores voluptas harum tenetur, id nihil adipisci suscipit
+      temporibus, quidem aliquam voluptates.
+    </p>
+  </article>
 
-    <div class="write">
-      <label for="textarea" />
-      <textarea
-        name="textarea"
-        id="textarea"
-        maxlength={myMaxLength}
-        on:input={handleInput}
-        placeholder="Modifié l'article"
+  <div class="write">
+    <label for="textarea" />
+    <textarea
+      name="textarea"
+      id="textarea"
+      maxlength={myMaxLength}
+      on:input={(handleInput, updatePreview)}
+      bind:value={text}      
+      spellcheck="true"
+      placeholder="Ecrire ici pour modifier l'article"
+    />
+    <p class="counter">
+      Nombre de caractères restant : <span id="characterCounter"
+        >{charactersRemaining}</span
+      >/<span id="maximum">{myMaxLength}</span>
+    </p>
+    <div class="buttons">
+      <input
+        class="submit"
+        type="submit"
+        name="submit"
+        value="Envoyer"
+        spellcheck="false"
+        aria-label="Envoyer article"
       />
-      <p class="counter">
-        Nombre de caractères restant : <span id="characterCounter"
-          >{charactersRemaining}</span
-        >/<span id="maximum">{myMaxLength}</span>
-      </p>
-      <div class="buttons">
-        <input
-          class="submit"
-          type="submit"
-          name="submit"
-          value="Envoyer"
-          spellcheck="false"
-          aria-label="Envoyer article"
-        />
-      </div>
-
     </div>
-    
   </div>
 </main>
 
 <style lang="scss">
-  .wrap {
-    display: block;
+  @import "../utils/extends";
+  @import "../utils/mixins";
+  @import "../utils/variables";
 
-    padding:3rem;
+  main {
+    padding: 3rem;
+    font-family: Tahoma;
 
-    .picture {
+    article {
       display: block;
-      width: 100%;
-      max-width: 100%;
-      height: auto;
-
-      box-shadow: 0 px 10px 0 rgb(31 38 135 / 45%);
-      backdrop-filter: blur(4px);
-      -webkit-backdrop-filter: blur(4px);
-      border-radius: 5px;
-      border: 1px solid rgba(255, 255, 255, 0.18);
-      img {
-        width: 100%;
-        height: 25rem;
-      }
-    }
-    h2 {
-      text-align: center;
-      padding: 2rem;
-      font-family: Tahoma;
-    }
-    p {
-      font-family: Tahoma;
-      padding: 2rem;
-    }
-
-    .article {
-      display: block;
-
       padding: 00.625rem;
-      box-shadow: 0 2px 10px 0 rgb(31 38 135 / 45%);
-      backdrop-filter: blur(4px);
-      -webkit-backdrop-filter: blur(4px);
-      border-radius: 5px;
-      border: 1px solid rgba(255, 255, 255, 0.18);
+      @extend %glassmorphism;
+
+      img {
+        min-width: 100%;
+        max-height: 160px;
+        object-fit: cover;
+        @extend %glassmorphism;
+      }
+
+      h2 {
+        text-align: center;
+        padding: 2rem;
+
+        font-weight: bolder;
+      }
+      #preview {
+        font-family: Tahoma;
+        padding: 2rem;
+        line-height: 2rem;
+      }
     }
     .write {
       display: block;
-
-      box-shadow: 0 2px 10px 0 rgb(31 38 135 / 45%);
-      backdrop-filter: blur(4px);
-      -webkit-backdrop-filter: blur(4px);
-      border-radius: 10px;
-      border: 1px solid rgba(255, 255, 255, 0.18);
+      @extend %glassmorphism;
       width: 100%;
       margin-top: 30px;
     }
@@ -153,47 +142,39 @@
       width: 95%;
       margin: 2rem auto;
       resize: vertical;
-      font-size: 1rem;
+      font-size: 1.6rem;
       min-height: 100px;
       border-radius: 5px;
       border: 1px solid #ccc;
       padding: 1rem;
-    }
-    .counter {
-      position: fixed;
-      bottom: 3.5rem;
-      left: 1rem;
-      font-size: 1.2rem;
-  
- 
-  
-      
-     
 
-      
+      textarea::-webkit-resizer,
+      textarea::-moz-resizer,
+      textarea::-o-resizer,
+      textarea::-ms-resizer {
+        height: 100px; /* masque l'icon de redimensionnement pour les différents navigateurs */
+      }
+    }
+
+    .counter {
+      position: relative;
+      left: 5rem;
+      font-size: 1.2rem;
     }
     .buttons {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  
-}
-
-.submit {
-  background-color:  rgba(136, 217, 143, 0.23);
-  color: rgb(19, 18, 18);
-  padding: 1rem 2rem;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  
-}
-      
-     
-            
-            
-      
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 1rem;
     }
-  
+
+    .submit {
+      background-color: rgba(136, 217, 143, 0.23);
+      color: rgb(19, 18, 18);
+      padding: 1rem 2rem;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  }
 </style>
