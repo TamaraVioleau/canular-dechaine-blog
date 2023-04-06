56d5768810d637d77fdfff1324d9164802a41916
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-06 15:04:25 +0200

    like done.

--- src/lib/PageArticle.svelte
+++ src/lib/PageArticle.svelte
@@ -1,33 +1,50 @@
 <script>
+  // Les fonctions onMount et onDestroy nous permettent de faire des choses spécifiques à des moments précis de l'application.
+  // onMount nous permet de faire quelque chose dès que l'application est prête à être utilisée
+  // onDestroy nous permet de faire quelque chose quand l'application se ferme ou qu'une partie de l'application est supprimée.
+  // Importe les fonctions onMount et onDestroy de la librairie Svelte
   import { onMount, onDestroy } from "svelte";
 
+  // Initialise les variables count et isActive en utilisant les données stockées dans localStorage, ou 0 et false si ces données ne sont pas encore présentes
   let count = localStorage.getItem("heartCount") || 0;
   let isActive = localStorage.getItem("heartActive") === "true";
 
+  // Fonction qui change l'état du coeur et met à jour le compteur de likes en fonction de l'état du coeur
   function toggleHeart() {
-  if (isActive) {
-    count -= 1;
-  } else {
-    count += 1;
+    if (isActive) {
+      count -= 1;
+    } else {
+      count += 1;
+    }
+    isActive = !isActive;
+    // Met à jour les données stockées dans localStorage avec les nouvelles valeurs de count et isActive
+    localStorage.setItem("heartCount", count);
+    localStorage.setItem("heartActive", isActive);
+    // Met à jour le texte du compteur de likes
+    document.querySelector(".heart-count").textContent = count;
   }
-  isActive = !isActive;
-  localStorage.setItem("heartCount", count);
-  localStorage.setItem("heartActive", isActive);
-  document.querySelector(".heart-count").textContent = count;
-}
 
+  // Utilise la fonction onMount pour exécuter du code dès que l'élément HTML est prêt à être utilisé
   onMount(() => {
+    // Sélectionne l'élément HTML contenant le coeur
     const heart = document.querySelector(".heart");
+    // Sélectionne l'élément HTML contenant le compteur de likes
     const countEl = document.querySelector(".heart-count");
+    // Met à jour le texte du compteur de likes avec la valeur actuelle de count
     countEl.textContent = count;
+    // Ajoute la classe "active" à l'élément HTML contenant le coeur si isActive est true
     if (isActive) {
       heart.classList.add("active");
     }
+    // Ajoute un event listener pour écouter les clics sur l'élément HTML contenant le coeur et appeler la fonction toggleHeart
     heart.addEventListener("click", toggleHeart);
   });
 
+  // Utilise la fonction onDestroy pour exécuter du code quand l'élément HTML est supprimé
   onDestroy(() => {
+    // Sélectionne l'élément HTML contenant le coeur
     const heart = document.querySelector(".heart");
+    // Supprime l'event listener pour éviter des fuites de mémoire
     heart.removeEventListener("click", toggleHeart);
   });
 </script>
@@ -35,11 +52,7 @@
 <main>
   <article class="article">
     <img src="https://picsum.photos/200" alt="foto" />
-    <div class="heart" class:active={isActive}>
-      <span class="heart-count" />
-      <i class="fa-regular fa-heart" id="heart-empty" />
-      <i class="fa-solid fa-heart hidden" id="heart-filled" />
-    </div>
+
     <h2>TITRE ARTICLE</h2>
     <p id="paragraph" aria-label="Texte de l'article">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies
@@ -100,6 +113,11 @@
         <time datetime="2023-04-05">5 avril 2023</time> <span> || </span>
         <cite title="nom de l'auteur">Sarah Croche</cite>
       </aside>
+      <div class="heart" class:active={isActive}>
+        <span class="heart-count" />
+        <i class="fa-regular fa-heart" id="heart-empty" />
+        <i class="fa-solid fa-heart hidden" id="heart-filled" />
+      </div>
     </footer>
   </article>
 </main>
@@ -124,36 +142,6 @@
         object-fit: cover;
         @extend %glassmorphism;
       }
-      .heart {
-  position: relative;
-  cursor: pointer;
-
-  i {
-    font-size: 1.5rem;
-    margin-right: 0.5rem;
-  }
-
-  #heart-filled {
-    display: none;
-  }
-
-  &.active #heart-empty {
-    display: none;
-  }
-
-  &.active #heart-filled {
-    display: inline-block;
-  }
-
-  .heart-count {
-    position: absolute;
-    left: -1.5rem;
-    top: 0.25rem;
-    font-size: 0.8rem;
-    font-weight: bold;
-  }
-}
-
 
       h2 {
         text-align: center;
@@ -165,11 +153,12 @@
         padding: 2rem;
         line-height: 2rem;
       }
+
       .footer__dateauthor {
         display: flex;
         padding: 1rem;
         margin: 1rem;
-        flex-direction: column;
+        justify-content: space-between;
         aside {
           display: flex;
           margin-right: 1rem;
@@ -181,6 +170,30 @@
           cite {
             font-style: normal;
             font-weight: bold;
+          }
+        }
+        .heart {
+          display: flex;
+          cursor: pointer;
+          font-size: 4rem;
+          color: rgb(229 62 62);
+          i {
+          }
+
+          #heart-filled {
+            display: none;
+          }
+
+          &.active #heart-empty {
+            display: none;
+          }
+
+          &.active #heart-filled {
+            display: inline-block;
+          }
+
+          .heart-count {
+            margin-right: 1rem;
           }
         }
       }
