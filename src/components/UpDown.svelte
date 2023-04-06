991cfc10526cbccaaf9a14541c5f43755396211d
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-06 17:33:49 +0200

    updown media querry done.

--- src/components/UpDown.svelte
+++ src/components/UpDown.svelte
@@ -20,53 +20,56 @@
   }
 </script>
 
-<button on:click={scrollToTop}><i class="fi fi-sr-caret-circle-up"></i></button>
-<button on:click={scrollToBottom}><i class="fi fi-sr-caret-circle-down"></i></button>
+<button on:click={scrollToTop}><i class="fi fi-sr-caret-circle-up" /></button>
+<button on:click={scrollToBottom}
+  ><i class="fi fi-sr-caret-circle-down" /></button
+>
 
 <style lang="scss">
   @import "../utils/extends";
   @import "../utils/mixins";
   @import "../utils/variables";
 
-  button {
-  display: flex;
-  align-items: center; // Centrer verticalement
-  justify-content: center; // Centrer horizontalement
-  z-index: 900000;
-  position: fixed;
-  right: 2rem;
-  width: 5rem;
-  height: 5rem;
-  font-size: 1.5rem;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 50%;
-  color: #666;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
+  @media screen and (min-width: 580px) {
+    button {
+      display: flex;
+      align-items: center; // Centrer verticalement
+      justify-content: center; // Centrer horizontalement
+      z-index: 900000;
+      position: fixed;
+      right: 2rem;
+      width: 5rem;
+      height: 5rem;
+      font-size: 1.5rem;
+      background-color: #fff;
+      border: 1px solid #ccc;
+      border-radius: 50%;
+      color: #666;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
 
-  &:hover {
-    transform: translateY(-5px);
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+      &:hover {
+        transform: translateY(-5px);
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+      }
+
+      &:focus {
+        outline: none;
+      }
+
+      &:first-of-type {
+        bottom: 8rem;
+      }
+
+      &:last-of-type {
+        bottom: 2rem;
+      }
+
+      i {
+        font-size: 3rem;
+        top: 0.8rem;
+        position: absolute;
+      }
+    }
   }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:first-of-type {
-    bottom: 8rem;
-  }
-
-  &:last-of-type {
-    bottom: 2rem;
-  }
-
-  i{
-    font-size: 3rem;
-    top: 0.8rem;
-    position: absolute;
-  }
-}
-
 </style>
