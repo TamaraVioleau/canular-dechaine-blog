c7574e83967c5f788f4de59be7a134f0dd055668
FredMorino <fred.morino73@gmail.com>
2023-04-06 11:56:14 +0200

    create router

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,7 @@
 <script>
-<<<<<<< HEAD
-  import Footer from "./lib/Footer.svelte";
-import Homepage from "./lib/HomePage.svelte"; 
-=======
   import ErrorPage from "./lib/ErrorPage.svelte";
   import Footer from "./lib/Footer.svelte";
   import PageArticles from "./lib/PageArticles.svelte";
->>>>>>> 1af4d8b51c08f1c6b15cfd15b56413c7f63b0ea5
   import PageProfilAuthors from "./lib/PageProfilAuthors.svelte";
   import PageProfilMembers from "./lib/PageProfilMembers.svelte";
   import Register from "./lib/Register.svelte";
@@ -15,28 +10,18 @@ import Homepage from "./lib/HomePage.svelte";
   import Navbar from "./lib/Navbar.svelte";
 </script>
 
-<<<<<<< HEAD
-<SearchPage/>
-
-<!-- <Homepage/>
-=======
 <Navbar/>
 <ErrorPage/>
 <Footer />
 
 <!--
   <SearchPage/>
->>>>>>> 1af4d8b51c08f1c6b15cfd15b56413c7f63b0ea5
 <PageProfilAuthors/>
 <PageProfilMembers/>
 <Register/>
 <PageArticles/>
  -->
-<<<<<<< HEAD
-<Footer />
-=======
 
 
 <style lang="scss">
 </style>
->>>>>>> 1af4d8b51c08f1c6b15cfd15b56413c7f63b0ea5
