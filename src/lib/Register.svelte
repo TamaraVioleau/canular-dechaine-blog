6588a4078a318bf10de818173829466248809a4b
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-12 19:05:50 +0200

    delete regex beaucause bug

--- src/lib/Register.svelte
+++ src/lib/Register.svelte
@@ -1,6 +1,5 @@
 <script>
   var details = document.querySelectorAll("details");
-
   details.forEach(function (detail) {
     detail.addEventListener("click", function () {
       details.forEach(function (otherDetail) {
@@ -10,73 +9,33 @@
       });
     });
   });
-
+  // Importation de la fonction "push" depuis le module "svelte-spa-router"
   import { push } from "svelte-spa-router";
+  // Déclaration de la variable "reload" initialisée à "false"
   export let reload = false;
-
+  // Déclaration des variables "email" et "password" pour le formulaire de login
   let email = "";
   let password = "";
-
-  const isValidEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-  const isValidPassword = (password) => {
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    return passwordRegex.test(password);
-  };
-
-  if (localStorage.getItem("token")) {
-    localStorage.removeItem("token");
+  // Vérifie si l'utilisateur est déjà connecté en vérifiant la présence du token d'authentification dans le local storage
+  const isLogged = window.localStorage.getItem("token") != null;
+  // Si l'utilisateur est déjà connecté, on supprime le token du local storage et on recharge la page
+  if (isLogged) {
+    window.localStorage.removeItem("token");
     location.reload();
   }
-
+  // Fonction appelée lors de la soumission du formulaire de login
   const handleSubmitForm = async (event) => {
-    event.preventDefault();
-
-    if (!isValidEmail(email) || !isValidPassword(password)) {
-      console.error("Invalid email or password");
-      return;
-    }
-
-    const { token, roleID } = await login(email, password);
-    localStorage.setItem("token", token);
-
-    if (reload) {
-      location.reload();
-    } else {
-      redirectToProfile(roleID);
-    }
-  };
-
-  const login = async (email, password) => {
-    const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/auth/login";
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
-    const json = await response.json();
-    const token = json.data.access_token;
-
-    const userResponse = await fetch(
-      import.meta.env.VITE_URL_DIRECTUS + "/users/me",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const userJson = await userResponse.json();
-    const roleID = userJson.data.role;
-
-    return { token, roleID };
-  };
-
-  const redirectToProfile = (roleID) => {
+  event.preventDefault(); // Empêche la soumission du formulaire
+  // Appelle la fonction "login" qui retourne un objet contenant le token d'authentification et l'ID du rôle
+  const { token, roleID } = await login(email, password);
+  // Enregistre le token d'authentification dans le local storage
+  window.localStorage.setItem("token", token);
+  console.log("Token:", token);
+  // Si la variable "reload" est vraie, on recharge la page
+  if (reload) {
+    location.reload();
+  } else {
+    // Redirige l'utilisateur vers la page de profil correspondante en fonction de l'ID du rôle
     if (roleID === "213b3c24-fb05-446d-ab79-fd05adbbd6e2") {
       push("/profil-membre");
     } else if (roleID === "645cbe7e-cdf9-409c-bc58-863ce065dfbb") {
@@ -84,57 +43,94 @@
     } else {
       console.error("Unknown role ID:", roleID);
     }
+  }
+};
+  // Fonction qui envoie une requête de login au serveur et retourne le token d'authentification et l'ID du rôle
+  const login = async (email, password) => {
+  const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/auth/login"; // URL de l'API de login
+  // Envoie une requête de type "POST" avec les données de login au format JSON
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+    email: email,
+    password: password,
+  }),
+  });
+  // Extrait le contenu de la réponse au format JSON
+  const json = await response.json();
+  // Récupère le token d'authentification depuis le JSON
+  const token = json.data.access_token;
+  // Envoie une requête pour obtenir les informations de l'utilisateur connecté avec le token d'authentification
+  const userResponse = await fetch(import.meta.env.VITE_URL_DIRECTUS + "/users/me", {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    },
+  });
+  // Extrait les informations de l'utilisateur connecté au format JSON
+  const userJson = await userResponse.json();
+  console.log("User data:", userJson);
+  // Récupère l'ID du rôle depuis les informations de l'utilisateur connecté
+  const roleID = userJson.data.role;
+  // Retourne un objet contenant le token d'authentification et l'ID du rôle
+  return { token, roleID };
+};
+  //////////// Register /////////////
+ // Initialisation des variables pour stocker les données du formulaire
+let pseudo = "";
+let mail = "";
+let pwd = "";
+// Fonction qui gère la soumission du formulaire
+const handleSubmit = async (event) => {
+  event.preventDefault(); // Empêche le formulaire d'être envoyé
+  const data = {
+    pseudo: pseudo,
+    mail: mail,
+    pwd: pwd,
   };
-
-  // Register part
-  let pseudo = "";
-  let mail = "";
-  let pwd = "";
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (!isValidEmail(mail) || !isValidPassword(pwd)) {
-      console.error("Invalid email or password");
-      return;
-    }
-
-    try {
-      await register({ pseudo, mail, pwd });
-      const { token, roleID } = await login(mail, pwd);
-      localStorage.setItem("token", token);
-      push("/profil-membre");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const register = async (data) => {
-    const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/users";
-    const membersRoleID = "213b3c24-fb05-446d-ab79-fd05adbbd6e2";
-
-    const response = await fetch(endpoint, {
+  try {
+    await register(data); // Appel de la fonction "register" qui envoie les données à l'API
+    const { token, roleID } = await login(mail, pwd); // Appelle la fonction "login" pour obtenir le jeton d'authentification et l'ID du rôle
+    window.localStorage.setItem("token", token); // Stockage du token dans le localStorage
+    console.log("Token:", token);
+    push("/profil-membre"); // Redirection vers la page du profil membre
+  } catch (error) {
+    console.error(error); // Affichage d'une erreur éventuelle dans la console
+  }
+};
+// Fonction qui envoie les données du formulaire à l'API pour enregistrer un nouvel utilisateur
+async function register(data) {
+  const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/users"; // URL de l'API pour enregistrer un nouvel utilisateur
+  const membersRoleID = "213b3c24-fb05-446d-ab79-fd05adbbd6e2"; // ID du rôle "members" dans l'API Directus
+  try {
+    const response = await fetch(endpoint, { // Envoi des données à l'API avec la méthode POST
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
+      headers: {
+        "Content-Type": "application/json", // Format des données envoyées
+      },
+      body: JSON.stringify({ // Encodage des données du formulaire en JSON
         email: data.mail,
         password: data.pwd,
         pseudo: data.pseudo,
         role: membersRoleID,
-        status: "active",
+        status: "active", // Statut du nouvel utilisateur
       }),
     });
-
-    if (response.ok) {
-      const json = await response.json();
-      const token = json.data.access_token;
-      return token;
+    if (response.ok) { // Si la réponse de l'API est OK
+      const json = await response.json(); // Extraction des données de la réponse au format JSON
+      const token = json.data.access_token; // Récupération du token d'authentification
+      return token; // Retourne le token pour qu'il soit stocké dans le localStorage
     } else {
-      throw new Error("Failed to register user");
+      throw new Error("Failed to register user"); // Sinon, affiche une erreur
     }
-  };
+  } catch (error) {
+    console.error(error); // Affiche une erreur éventuelle dans la console
+  }
+}
 </script>
-
 <main>
   <wrapper class="wrapper--left">
     <!-- dans action mettre le nom de la page (ex: /profil)  -->
@@ -147,7 +143,6 @@
     >
       <section class="section--login" aria-labelledby="login">
         <h1 id="login">Se connecter</h1>
-
         <details open>
           <summary>Voir le formulaire</summary>
           <article class="article--login" aria-label="formulaire de connexion">
@@ -189,7 +184,6 @@
       </section>
     </form>
   </wrapper>
-
   <wrapper class="wrapper--right"
     ><form
       on:submit={handleSubmit}
@@ -200,7 +194,6 @@
     >
       <section class="section--register" aria-labelledby="register">
         <h1 id="statistiques">S'enregistrer :</h1>
-
         <details>
           <summary>Voir le formulaire</summary>
           <article
@@ -228,7 +221,7 @@
               type="password"
               name="pwd"
               id="pwd"
-              bind:value={pwd}
+              bind:value={pwd}            
               required
             />
           </article>
@@ -254,15 +247,12 @@
     </form>
   </wrapper>
 </main>
-
 <style lang="scss">
   @import "../utils/extends";
   @import "../utils/mixins";
   @import "../utils/variables";
-
   main {
     @extend %blocprofilregister;
-
     .wrapper--left {
       min-width: 390px;
       form {
@@ -280,7 +270,6 @@
           min-width: 40%;
           min-width: 390px;
         }
-
         .section--login {
           @extend %glassmorphism;
           @extend %paddingprofilregister;
@@ -298,7 +287,6 @@
               cursor: pointer;
               font-weight: bold;
             }
-
             .article--login {
               margin-top: 2rem;
               padding: 1.5rem;
@@ -306,7 +294,6 @@
               flex-direction: column;
               max-width: 500px;
               margin: auto;
-
               label {
                 padding: 1rem 0;
                 font-weight: bolder;
@@ -325,7 +312,6 @@
             justify-content: center;
             margin-top: 2rem;
             gap: 1.5rem;
-
             input {
               @extend %inputformbutton;
             }
@@ -333,7 +319,6 @@
         }
       }
     }
-
     .wrapper--right {
       min-width: 390px;
       form {
@@ -358,7 +343,6 @@
           @media screen and (min-width: 1024px) {
             margin-top: 0;
           }
-
           h1 {
             font-size: 3rem;
             font-weight: bolder;
@@ -367,14 +351,12 @@
             line-height: 3rem;
             justify-content: center;
           }
-
           details {
             summary {
               padding: 2rem;
               cursor: pointer;
               font-weight: bold;
             }
-
             .article--register {
               margin-top: 2rem;
               padding: 1.5rem;
@@ -386,7 +368,6 @@
                 padding: 1rem 0;
                 font-weight: bolder;
               }
-
               input {
                 padding: 1rem;
                 background: $color-greenlight;
@@ -401,7 +382,6 @@
             justify-content: center;
             margin-top: 2rem;
             gap: 1.5rem;
-
             input {
               @extend %inputformbutton;
             }
