07288f15c80d0204e2b7dc37c6e65120d40ac734
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-15 22:43:44 +0200

    update register

--- src/lib/Register.svelte
+++ src/lib/Register.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { link } from "svelte-spa-router";
   var details = document.querySelectorAll("details");
   details.forEach(function (detail) {
     detail.addEventListener("click", function () {
@@ -25,112 +26,119 @@
   }
   // Fonction appelée lors de la soumission du formulaire de login
   const handleSubmitForm = async (event) => {
-  event.preventDefault(); // Empêche la soumission du formulaire
-  // Appelle la fonction "login" qui retourne un objet contenant le token d'authentification et l'ID du rôle
-  const { token, roleID } = await login(email, password);
-  // Enregistre le token d'authentification dans le local storage
-  window.localStorage.setItem("token", token);
-  console.log("Token:", token);
-  // Si la variable "reload" est vraie, on recharge la page
-  if (reload) {
-    location.reload();
-  } else {
-    // Redirige l'utilisateur vers la page de profil correspondante en fonction de l'ID du rôle
-    if (roleID === "213b3c24-fb05-446d-ab79-fd05adbbd6e2") {
-      push("/profil-membre");
-    } else if (roleID === "645cbe7e-cdf9-409c-bc58-863ce065dfbb") {
-      push("/profil-auteur");
+    event.preventDefault(); // Empêche la soumission du formulaire
+    // Appelle la fonction "login" qui retourne un objet contenant le token d'authentification et l'ID du rôle
+    const { token, roleID } = await login(email, password);
+    // Enregistre le token d'authentification dans le local storage
+    window.localStorage.setItem("token", token);
+    console.log("Token:", token);
+    // Si la variable "reload" est vraie, on recharge la page
+    if (reload) {
+      location.reload();
     } else {
-      console.error("Unknown role ID:", roleID);
+      // Redirige l'utilisateur vers la page de profil correspondante en fonction de l'ID du rôle
+      if (roleID === "213b3c24-fb05-446d-ab79-fd05adbbd6e2") {
+        push("/profil-membre");
+      } else if (roleID === "645cbe7e-cdf9-409c-bc58-863ce065dfbb") {
+        push("/profil-auteur");
+      } else {
+        console.error("Unknown role ID:", roleID);
+      }
     }
-  }
-};
+  };
   // Fonction qui envoie une requête de login au serveur et retourne le token d'authentification et l'ID du rôle
   const login = async (email, password) => {
-  const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/auth/login"; // URL de l'API de login
-  // Envoie une requête de type "POST" avec les données de login au format JSON
-  const response = await fetch(endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-    email: email,
-    password: password,
-  }),
-  });
-  // Extrait le contenu de la réponse au format JSON
-  const json = await response.json();
-  // Récupère le token d'authentification depuis le JSON
-  const token = json.data.access_token;
-  // Envoie une requête pour obtenir les informations de l'utilisateur connecté avec le token d'authentification
-  const userResponse = await fetch(import.meta.env.VITE_URL_DIRECTUS + "/users/me", {
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`,
-    },
-  });
-  // Extrait les informations de l'utilisateur connecté au format JSON
-  const userJson = await userResponse.json();
-  console.log("User data:", userJson);
-  // Récupère l'ID du rôle depuis les informations de l'utilisateur connecté
-  const roleID = userJson.data.role;
-  // Retourne un objet contenant le token d'authentification et l'ID du rôle
-  return { token, roleID };
-};
-  //////////// Register /////////////
- // Initialisation des variables pour stocker les données du formulaire
-let pseudo = "";
-let mail = "";
-let pwd = "";
-// Fonction qui gère la soumission du formulaire
-const handleSubmit = async (event) => {
-  event.preventDefault(); // Empêche le formulaire d'être envoyé
-  const data = {
-    pseudo: pseudo,
-    mail: mail,
-    pwd: pwd,
-  };
-  try {
-    await register(data); // Appel de la fonction "register" qui envoie les données à l'API
-    const { token, roleID } = await login(mail, pwd); // Appelle la fonction "login" pour obtenir le jeton d'authentification et l'ID du rôle
-    window.localStorage.setItem("token", token); // Stockage du token dans le localStorage
-    console.log("Token:", token);
-    push("/profil-membre"); // Redirection vers la page du profil membre
-  } catch (error) {
-    console.error(error); // Affichage d'une erreur éventuelle dans la console
-  }
-};
-// Fonction qui envoie les données du formulaire à l'API pour enregistrer un nouvel utilisateur
-async function register(data) {
-  const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/users"; // URL de l'API pour enregistrer un nouvel utilisateur
-  const membersRoleID = "213b3c24-fb05-446d-ab79-fd05adbbd6e2"; // ID du rôle "members" dans l'API Directus
-  try {
-    const response = await fetch(endpoint, { // Envoi des données à l'API avec la méthode POST
+    const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/auth/login"; // URL de l'API de login
+    // Envoie une requête de type "POST" avec les données de login au format JSON
+    const response = await fetch(endpoint, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json", // Format des données envoyées
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ // Encodage des données du formulaire en JSON
-        email: data.mail,
-        password: data.pwd,
-        pseudo: data.pseudo,
-        role: membersRoleID,
-        status: "active", // Statut du nouvel utilisateur
+      body: JSON.stringify({
+        email: email,
+        password: password,
       }),
     });
-    if (response.ok) { // Si la réponse de l'API est OK
-      const json = await response.json(); // Extraction des données de la réponse au format JSON
-      const token = json.data.access_token; // Récupération du token d'authentification
-      return token; // Retourne le token pour qu'il soit stocké dans le localStorage
-    } else {
-      throw new Error("Failed to register user"); // Sinon, affiche une erreur
+    // Extrait le contenu de la réponse au format JSON
+    const json = await response.json();
+    // Récupère le token d'authentification depuis le JSON
+    const token = json.data.access_token;
+    // Envoie une requête pour obtenir les informations de l'utilisateur connecté avec le token d'authentification
+    const userResponse = await fetch(
+      import.meta.env.VITE_URL_DIRECTUS + "/users/me",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    // Extrait les informations de l'utilisateur connecté au format JSON
+    const userJson = await userResponse.json();
+    console.log("User data:", userJson);
+    // Récupère l'ID du rôle depuis les informations de l'utilisateur connecté
+    const roleID = userJson.data.role;
+    // Retourne un objet contenant le token d'authentification et l'ID du rôle
+    return { token, roleID };
+  };
+  //////////// Register /////////////
+  // Initialisation des variables pour stocker les données du formulaire
+  let pseudo = "";
+  let mail = "";
+  let pwd = "";
+  // Fonction qui gère la soumission du formulaire
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Empêche le formulaire d'être envoyé
+    const data = {
+      pseudo: pseudo,
+      mail: mail,
+      pwd: pwd,
+    };
+    try {
+      await register(data); // Appel de la fonction "register" qui envoie les données à l'API
+      const { token, roleID } = await login(mail, pwd); // Appelle la fonction "login" pour obtenir le jeton d'authentification et l'ID du rôle
+      window.localStorage.setItem("token", token); // Stockage du token dans le localStorage
+      console.log("Token:", token);
+      push("/profil-membre"); // Redirection vers la page du profil membre
+    } catch (error) {
+      console.error(error); // Affichage d'une erreur éventuelle dans la console
     }
-  } catch (error) {
-    console.error(error); // Affiche une erreur éventuelle dans la console
+  };
+  // Fonction qui envoie les données du formulaire à l'API pour enregistrer un nouvel utilisateur
+  async function register(data) {
+    const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/users"; // URL de l'API pour enregistrer un nouvel utilisateur
+    const membersRoleID = "213b3c24-fb05-446d-ab79-fd05adbbd6e2"; // ID du rôle "members" dans l'API Directus
+    try {
+      const response = await fetch(endpoint, {
+        // Envoi des données à l'API avec la méthode POST
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Format des données envoyées
+        },
+        body: JSON.stringify({
+          // Encodage des données du formulaire en JSON
+          email: data.mail,
+          password: data.pwd,
+          pseudo: data.pseudo,
+          role: membersRoleID,
+          status: "active", // Statut du nouvel utilisateur
+        }),
+      });
+      if (response.ok) {
+        // Si la réponse de l'API est OK
+        const json = await response.json(); // Extraction des données de la réponse au format JSON
+        const token = json.data.access_token; // Récupération du token d'authentification
+        return token; // Retourne le token pour qu'il soit stocké dans le localStorage
+      } else {
+        throw new Error("Failed to register user"); // Sinon, affiche une erreur
+      }
+    } catch (error) {
+      console.error(error); // Affiche une erreur éventuelle dans la console
+    }
   }
-}
 </script>
+
 <main>
   <wrapper class="wrapper--left">
     <!-- dans action mettre le nom de la page (ex: /profil)  -->
@@ -221,9 +229,27 @@ async function register(data) {
               type="password"
               name="pwd"
               id="pwd"
-              bind:value={pwd}            
+              bind:value={pwd}
               required
             />
+
+            <fieldset>
+              <legend>Cocher pour valider votre inscription :</legend>
+              <input
+                id="conditions d'utilisations"
+                type="checkbox"
+                name="conditions d'utilisations"
+                value="conditions d'utilisations"
+                required
+              />
+              <label for="conditions d'utilisations" id="conditions"
+                >J'ai lu et j'accepte les <a
+                  use:link
+                  href="/conditions-utilisations"
+                  >présentes conditions d'utilisation</a
+                ></label
+              >
+            </fieldset>
           </article>
           <div class="buttons">
             <input
@@ -247,6 +273,7 @@ async function register(data) {
     </form>
   </wrapper>
 </main>
+
 <style lang="scss">
   @import "../utils/extends";
   @import "../utils/mixins";
@@ -364,7 +391,14 @@ async function register(data) {
               flex-direction: column;
               max-width: 500px;
               margin: auto;
-              label {
+              fieldset {
+                margin-top: 2rem;
+              }
+              #conditions {
+                font-weight: 500;
+              }
+              label,
+              legend {
                 padding: 1rem 0;
                 font-weight: bolder;
               }
--- src/lib/SearchPage.svelte
+++ src/lib/SearchPage.svelte
@@ -1,6 +1,5 @@
 <script>
   import { link } from "svelte-spa-router";
-
 </script>
 
 <main>
