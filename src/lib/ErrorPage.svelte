02b38ff427598e8bcbf6b735e416d5399a04b706
Tamara Violeau <103538862+TamaraVioleau@users.noreply.github.com>
2023-04-17 10:03:23 +0200

    modification error page

--- src/lib/ErrorPage.svelte
+++ src/lib/ErrorPage.svelte
@@ -7,9 +7,8 @@
             <div class="row">	
             <div class="col-sm-12 ">
             <div class="col-sm-10 col-sm-offset-1  text-center">
-            <div class="four_zero_four_bg">
-                <h1 class="text-center ">404</h1>
-            
+           <h1 class="text-center ">404</h1> 
+           <div class="four_zero_four_bg">      
             
             </div>
             
@@ -45,7 +44,7 @@
  }
  
  
- .four_zero_four_bg h1{
+ h1{
  font-size:80px;
  text-align: center;
  
