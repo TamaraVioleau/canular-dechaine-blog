3594a08750acc27dc5e09837b348a38f4ad3a9bf
FredMorino <fred.morino73@gmail.com>
2023-04-19 14:13:41 +0200

    update comment modifiedArticle

--- src/lib/ModifiedArticle.svelte
+++ src/lib/ModifiedArticle.svelte
@@ -1,35 +1,61 @@
 <script>
+  // Importe le composant TextareaModification depuis le fichier TextareaModificationArticle.svelte
   import TextareaModification from "../components/TextareaModificationArticle.svelte";
 
-
+  // Exporte une variable params pour permettre de recevoir des paramètres externes.
   export let params = {};
+
+  // Récupère l'ID de l'article depuis les paramètres.
   const article_id = params.article_id;
 
+  // Fonction asynchrone getArticle qui récupère un article en fonction de son ID.
   const getArticle = async (id) => {
-    const endpoint = import.meta.env.VITE_URL_DIRECTUS + "/items/articles/" + id;
+    // Construit l'URL de l'API pour récupérer l'article correspondant à l'ID donné.
+    const endpoint =
+      import.meta.env.VITE_URL_DIRECTUS + "/items/articles/" + id;
+
+    // Effectue une requête fetch vers l'URL de l'API.
     const response = await fetch(endpoint);
-    console.log("response", response);
+    //console.log("response", response);
+
+    // Attend la réponse et la convertit en objet JSON.
     const json = await response.json();
-    console.log("json", json);
+    //console.log("json", json);
+
+    // Retourne les données de l'article
     return json.data;
   };
-
 </script>
 
 <main>
+  <!-- Utilise la syntaxe await pour attendre que la fonction getArticle soit résolue avec l'ID de l'article. -->
   {#await getArticle(article_id)}
-  <p>En chargement. Je cherche les données sur l'api...</p>
-{:then article}
-  <article>
-    <img  src={import.meta.env.VITE_URL_DIRECTUS + "/assets/" + article.image}
-    alt={article.alt}/>
-    <h3>{article.title}</h3>
-    <p id="preview" aria-label="Texte de l'article" aria-live="polite" aria-atomic="true">
-      {article.content}
-    </p>
-  </article>
+  
+    <!-- Affiche un message pendant le chargement des données de l'article. -->
+    <p>En chargement. Je cherche les données sur l'api...</p>
+
+    <!-- Une fois la promesse résolue, récupère et affiche les informations de l'article. -->
+  {:then article}
+    <article>
+      <img
+        src={import.meta.env.VITE_URL_DIRECTUS + "/assets/" + article.image}
+        alt={article.alt}
+      />
+      <h3>{article.title}</h3>
+      <p
+        id="preview"
+        aria-label="Texte de l'article"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {article.content}
+      </p>
+    </article>
+
+    <!-- Ferme la syntaxe await -->
   {/await}
 
+  <!-- Inclut le composant TextareaModification en passant les paramètres reçus. -->
   <TextareaModification {params} />
 </main>
 
@@ -57,7 +83,7 @@
       padding: 2rem;
       margin: 3rem;
       @extend %glassmorphism;
-    @media screen and (min-width: 770px) {
+      @media screen and (min-width: 770px) {
         padding-inline: 17vw;
         padding-top: 5rem;
       }
